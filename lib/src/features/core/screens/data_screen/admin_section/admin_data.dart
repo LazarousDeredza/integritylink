58756928c27915af0265b16662a41161657ed3a8
Lazarous Deredza <lazarousderedza99@gmail.com>
2023-10-09 14:07:50 +0200

    dgdg

--- lib/src/features/core/screens/data_screen/admin_section/admin_data.dart
+++ lib/src/features/core/screens/data_screen/admin_section/admin_data.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:integritylink/src/constants/colors.dart';
 import 'package:integritylink/src/features/core/screens/data_screen/admin_section/data_list_admin.dart';
-import 'package:integritylink/src/features/core/screens/data_screen/data_list.dart';
 import 'package:integritylink/src/features/core/screens/group_chat/helper/helper_function.dart';
 import 'package:integritylink/src/repository/authentication_repository/authentication_repository.dart';
 
